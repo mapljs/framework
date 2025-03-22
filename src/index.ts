60b5214294a3cda2918a67189554e28cb756d745
aquapi <aquaplmc@gmail.com>
2025-03-22 22:52:51 +0700

    Update typings

--- src/index.ts
+++ src/index.ts
@@ -74,9 +74,9 @@ export type Hook<T> = (
 /**
  * Hooks for compilation
  */
-export type CompilerHooks<E extends ErrorHandler = ErrorHandler, T extends Handler = Handler> = [
-  compileHandler: Hook<T>,
-  compileErrorHandler: Hook<E>
+export type CompilerHooks<E extends ErrorFunc = ErrorFunc, T extends Func = Func> = [
+  compileHandler: Hook<Handler<T>>,
+  compileErrorHandler: Hook<ErrorHandler<E>>
 ];
 
 /**
