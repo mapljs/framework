7c57460564792753e0a16ef5a88b3bde17d24e99
aquapi <aquaplmc@gmail.com>
2025-09-09 19:21:14 +0700

    Update

--- src/config.ts
+++
@@ -1,6 +0,0 @@
-let _hydrateDependency = true;
-
-export const hydrateDependency = (): boolean => _hydrateDependency;
-export const noHydrateDependency = (): void => {
-  _hydrateDependency = false;
-}
